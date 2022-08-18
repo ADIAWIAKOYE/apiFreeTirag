5677c67bb86f402107c78c741af660e7c470160a
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-08-18 15:34:56 +0000

    Initial commit

---
+++ src/main/java/com/tirage/apifreetirage/ApiFreeTirageApplication.java
@@ -0,0 +1,13 @@
+package com.tirage.apifreetirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFreeTirageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFreeTirageApplication.class, args);
+    }
+
+}
