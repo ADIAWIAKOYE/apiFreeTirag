7580d43eb6484421b543cfaac149f42584f202e9
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-08-19 11:58:29 +0000

    ajout des attribut et entites dans liste et postulant

--- src/main/java/com/tirage/apifreetirage/modele/Postulant.java
+++ src/main/java/com/tirage/apifreetirage/modele/Postulant.java
@@ -1,4 +1,24 @@
 package com.tirage.apifreetirage.modele;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "postulant")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Postulant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_postulant;
+    private String nom_postulant;
+    private String prenom_postulant;
+    private String numero_postulant;
+    //@Email(message = "Wrong e-mail")
+    private String email_postulant;
 }
