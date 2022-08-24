8c8c2250352fe8b91ca4e0228333fba00550abeb
keita <kmahamadou858@gmail.com>
2022-08-24 11:43:52 +0200

    le recuperation des postulant d'un trie donnée

--- src/main/java/com/tirage/apifreetirage/controller/PostulantTrieController.java
+++ src/main/java/com/tirage/apifreetirage/controller/PostulantTrieController.java
@@ -2,9 +2,12 @@ package com.tirage.apifreetirage.controller;
 
 import com.tirage.apifreetirage.modele.PostulantTrie;
 import com.tirage.apifreetirage.services.PostulantTrieService;
+import com.tirage.apifreetirage.services.TirageService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,7 @@ public class PostulantTrieController {
 
     @Autowired
     private final PostulantTrieService postulantTrieService;//definition du service des postulant triés
+    private final TirageService tirageService;
 
     //methode permettant de créer un postulant trié dans la table postulant trié
     public int create(long idpostulant, String nom_postulant, String prenom_postulant, long numero_postulant, String email, long idtirage_id){
@@ -24,5 +28,11 @@ public class PostulantTrieController {
         return postulantTrieService.creer(idpostulant, nom_postulant, prenom_postulant,numero_postulant, email,idtirage_id);
     }
 
+
+    @GetMapping("/recupererPostulant")
+    public List<PostulantTrie> recupererPostulantTire(String libellet){
+
+        return postulantTrieService.trouverPostulantTrieParIdtirage(tirageService.trouverTirageParLibelle(libellet).getId());
+    }
 
 }
